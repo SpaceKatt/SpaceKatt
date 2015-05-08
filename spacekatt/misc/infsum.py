ac74dd58530757f165040b45d825de83893ee1bf
SpaceKatt <spacekattpoispin@gmail.com>
2015-05-08 21:29:00 +0000

    Taylor Series: Now have (x - a) ** n, factorial, and n-deriv at a, along with auto-simplification. Mostly presented as string lists. More change to come as ways to further simplify show themselves.

--- spacekatt/misc/infsum.py
+++ spacekatt/misc/infsum.py
@@ -25,22 +25,58 @@ def ndiff(fx, n):
 	else:
 		return terms
 
-gblogs = {'x': 0}
+#gblogs = {'x': 0}
 
-def whaha(fx, n):
-	for z in range(n):
-		macl = eval('ndiff(fx, n)', gblogs)
-		return macl
+#def whaha(fx, n):
+#	for z in range(n):
+#		macl = eval('ndiff(fx, n)', gblogs)
+#		return macl
 
 
 def funn():
 	return 2
 
+def evalu(fx, input):
+	x = input
+	a = eval(fx)
+	return a
+
+def taylorcn(eq, terms, a):
+	cnof = []
+	listfx = ndiff(eq, terms)
+	factori = factz(terms)
+	for z in range(0, len(listfx)):
+		cn = evalu(str(listfx[z]), a)
+		thing = '{0} / {1}'.format(cn, factori[z])
+		cnof.append(thing)
+	return cnof
+
+def simplify(list):
+	simple = []
+	a = 0
+	for n in range(0, len(list)):
+		m = eval(list[n + a])
+		if m == 0:
+			list.pop(n + a)
+			a -= 1
+		if m != 0:
+			continue
+	return list
+
+def xterm(terms, a):
+	xtermz = []
+	for i in range(0, terms):
+		thing = '((x - {0}) ** {1})'.format(a, i)
+		xtermz.append(thing)
+	return xtermz
 
 m = funn()
 
 eq = x ** 4
 print ndiff(eq, 4)
+
 #print whaha(sin(x), 2)
 
-print factz(5)
+ser = taylorcn(eq, 4, 1)
+print simplify(ser)
+print xterm(4, 1)
