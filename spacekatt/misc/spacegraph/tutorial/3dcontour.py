463e90ce5146c105674ddb3d40f4276226f9415b
SpaceKatt <spacekattpoispin@gmail.com>
2015-05-08 08:35:56 +0000

    More Graph tutorials completed. Retyping these helps me work through each part sequentially, and will most likely be handy for future reference.

---
+++ spacekatt/misc/spacegraph/tutorial/3dcontour.py
@@ -0,0 +1,25 @@
+import matplotlib as mpl
+mpl.use('agg')
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+from itertools import product, combinations
+import numpy as np
+import pylab
+from matplotlib import cm
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+X, Y, Z = axes3d.get_test_data(0.05)
+ax.plot_surface(X, Y, Z, rstride=8, cstride=8, alpha=0.3)
+cset = ax.contour(X, Y, Z, zdir='z', offset=-100, cmap=cm.coolwarm)
+cset = ax.contour(X, Y, Z, zdir='x', offset=-40, cmap=cm.coolwarm)
+cset = ax.contour(X, Y, Z, zdir='y', offset=40, cmap=cm.coolwarm)
+
+ax.set_xlabel('X')
+ax.set_xlim(-40, 40)
+ax.set_ylabel('Y')
+ax.set_ylim(-40, 40)
+ax.set_zlabel('Z')
+ax.set_zlim(-100, 100)
+
+plt.savefig('3dcontour.png')
